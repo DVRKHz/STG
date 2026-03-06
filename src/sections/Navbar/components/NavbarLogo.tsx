203b764173fa3adf935ebea62bd368c3d7023757
Rodrigo Martinez <rear097@gmail.com>
2026-03-06 13:08:04 -0600

    another fix attempt

--- src/sections/Navbar/components/NavbarLogo.tsx
+++ src/sections/Navbar/components/NavbarLogo.tsx
@@ -8,8 +8,8 @@ export const NavbarLogo = () => {
             className="text-black box-border caret-transparent inline-block md:text-pink-600 hover:text-slate-700 hover:border-slate-700"
           >
             <img
-              src="/logo1.png"
-              alt="Logo"
+              src="src/assets/logo-cedes.png"
+              alt="icon"
               sizes="(max-width: 100px) 100vw, 100px"
               className="h-10 w-auto object-contain"
             />
